4f8b5036f099a55ac376b2f9c59b8af894ea99af
chuckrapp <chuck.r.rapp>
2017-05-16 10:37:20 -0400

    hopefully no conflicts!

--- src/js/components/PlayerView/CurrentLeader.js
+++ src/js/components/PlayerView/CurrentLeader.js
@@ -1,13 +1,27 @@
 import React from "react";
-
+import Request from "superagent";
 
 export default class CurrentLeader extends React.Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
+
+  componentWillMount() {
+    var url = 'localhost:1337/api/currentLeader';
+    Request.get(url).then((response) => {
+      this.setState({
+        leader: response
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="col-md-12">
           <h4>Current Leader</h4> {/*static info*/}
-          <h2 id='currentLead'>LEADER HERE!</h2> {/*should be info from database*/}
+          <h2 id='currentLead'>{this.state.leader}</h2> {/*should be info from database*/}
         </div>
       </div>
     );
--- src/js/components/PlayerView/PlayerStats.js
+++ src/js/components/PlayerView/PlayerStats.js
@@ -2,6 +2,7 @@ import React from "react";
 
 
 export default class PlayerStats extends React.Component {
+  
   render() {
     return (
       <div>
